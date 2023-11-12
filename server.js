97f51f9faf040ccccfd0336e9e0063fc27d93934
NordyPi <loklinnord77@gmail.com>
2023-11-12 10:20:46 -0700

    updated to https

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var http = require('http');
+var https = require('https');
 
 var host = '192.168.50.22'
-var port = 80;
+var port = 443;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end("My first server!");
 };
 
-const server = http.createServer(requestListener);
+const server = https.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
